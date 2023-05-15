9bf8b79793e73167ebea0e9e27d1fe260d811cb0
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-15 09:22:31 +0500

    New Js Added

--- Assignment 07/Task 01/script.js
+++ Assignment 07/Task 01/script.js
@@ -1,7 +1,7 @@
-  
+
 function checkPassword() {
     var inputText = document.getElementById("inputPassword").value;
     var strengthChecker = inputText.length;
-    document.getElementById("toDisplay").innerHTML = `Length is ${strengthChecker}`;
-  }
+    document.getElementById("toDisplay").innerHTML = `<p>Length is ${strengthChecker}</p>`;
+}
 
