3fcb1ef0e85ce00dca759fe8d6502726589349f7
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-21 17:41:45 +0500

    Task 03 Done

---
+++ Js Tasks/Task 03/script.js
@@ -0,0 +1,32 @@
+function passwordChecker(){
+
+  var inputValue = document.getElementById("input").value
+  var lengthChecker = inputValue.length;
+   var checkUpperCase = /[A-Z]/.test(inputValue);
+   var checkLowerCase = /[a-z]/.test(inputValue);
+   var checkNumber = /[0-9]/.test(inputValue);
+
+   if (lengthChecker < 8) {
+     document.getElementById("toDisplay").innerHTML = `<p>The Password has less than 8 Characters.</p>`
+   }
+     else if (!checkUpperCase){
+      document.getElementById("toDisplay").innerHTML = `<p>The Password does not has Uppercase letter.</p>`
+
+     }
+     else if (!checkLowerCase){
+      document.getElementById("toDisplay").innerHTML = `<p>The Password does not has Lowecase letter.</p>`
+
+     }
+     else if(!checkNumber){
+      document.getElementById("toDisplay").innerHTML = `<p>The Password does not has Number.</p>`
+
+     }
+    
+   else{
+    document.getElementById("toDisplay").innerHTML = `<p>The Password is correct.</p>`
+
+   }
+   
+
+
+}
