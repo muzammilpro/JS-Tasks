bca7c8fe34be492157cf250c404be36c81db18df
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-23 09:50:37 +0500

    Do Some changes

---
+++ Assignment 07/Task 07/script.js
@@ -0,0 +1,9 @@
+function countDown() {
+    var hours = document.getElementById("hours").value
+    var minutes = document.getElementById("minutes").value
+    var second = document.getElementById("seconds").value
+
+    
+
+
+}
