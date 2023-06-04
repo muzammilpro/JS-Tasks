7abb8113ac468db0f61711c0c83de4f4e055ea91
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-06-04 21:46:12 +0500

    Do Some Changes

---
+++ Js Tasks/Task 05/script.js
@@ -0,0 +1,9 @@
+var inputStudents = [
+  { name: 'naveed', dob: '10-11-1997', email: 'naveed@techloset.com' },
+  { name: 'ali', dob: '10-11-1996', email: 'ali@techloset.com' },
+  { name: 'zain', dob: '10-12-1995', email: 'zain@techloset.com' }
+]
+
+// var outputArray = inputStudents.map()
+
+var outArrayStudents = inputStudents.map(function(student) 
